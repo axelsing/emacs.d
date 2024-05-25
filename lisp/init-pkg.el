365b18ab048d18ffce0cb43bba665184016b5221
chengjie2 <chengjie2@huya.com>
2024-05-25 18:20:50 +0800

    ivy

--- lisp/init-pkg.el
+++ lisp/init-pkg.el
@@ -118,17 +118,39 @@
 (global-set-key (kbd "C-x p") 'fzf-git-files)
 (global-set-key (kbd "C-x f") 'fzf-git-files)
 
+;; (use-package ivy
+;;   :defer 1
+;;   :demand
+;;   :hook (after-init . ivy-mode)
+;;   :config
+;;   (ivy-mode 1)
+;;   (setq ivy-use-virtual-buffers t
+;;         ivy-initial-inputs-alist nil
+;;         ivy-count-format "%d/%d "
+;;         enable-recursive-minibuffers t
+;;         ivy-re-builders-alist '((t . ivy--regex-ignore-order))))
+
 (use-package ivy
-  :defer 1
-  :demand
-  :hook (after-init . ivy-mode)
-  :config
+  :ensure t
+  :init
   (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t
-        ivy-initial-inputs-alist nil
-        ivy-count-format "%d/%d "
-        enable-recursive-minibuffers t
-        ivy-re-builders-alist '((t . ivy--regex-ignore-order))))
+  (counsel-mode 1)
+  :custom
+  (ivy-use-virtual-buffers t)
+  (search-default-mode #'char-fold-to-regexp)
+  (ivy-count-format "(%d/%d) ")
+  :bind
+  (("C-s" . 'swiper)
+   ("C-x b" . 'ivy-switch-buffer)
+   ("C-x C-b" . 'ivy-switch-buffer)
+   ("C-c v" . 'ivy-push-view)
+   ("C-c s" . 'ivy-switch-view)
+   ("C-c V" . 'ivy-pop-view)
+   ("C-x C-@" . 'counsel-mark-ring); 在某些终端上 C-x C-SPC 会被映射为 C-x C-@，比如在 macOS 上，所以要手动设置
+   ("C-x C-SPC" . 'counsel-mark-ring)
+   :map minibuffer-local-map
+   ("C-r" . counsel-minibuffer-history)))
+
 
 (use-package counsel
   :after (ivy)
@@ -143,6 +165,10 @@
          ("C-r" . swiper-isearch-backward))
   :config (setq swiper-action-recenter t
                 swiper-include-line-number-in-search t))
+
+(use-package amx
+  :ensure t
+  :init (amx-mode))
 
 (use-package ace-window
   :ensure t
